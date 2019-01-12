4db8e7be43f20e13176472ac401b64f1ac499065
leotrs <leo@leotrs.com>
2019-01-12 09:27:51 -0500

    set package name as netrd and make this directory into a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='netrd'
+    version='0.0.1',
+    author='NetSI 2019 Collabathon Team',
+    author_email='leo@leotrs.com',
+    description='Repository of network reconstruction and distance methods',
+    url='https://github.com/netsiphd/scale-free-networks-are-awesome',
+    packages=setuptools.find_packages(),
+    classifiers=['Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent']
+)

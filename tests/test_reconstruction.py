470adb21d922aea68a0dcf8795df93ef89ca01de
sdmccabe@github.com <stefanmccabe@gmail.com>
2019-01-14 13:53:34 -0500

    dummy test for reconstruction

---
+++ tests/test_reconstruction.py
@@ -0,0 +1,22 @@
+"""
+test_reconstruction.py
+----------------------
+
+Test reconstruction algorithms.
+
+"""
+
+import numpy as np
+from netrd import reconstruction
+
+
+def test_graph_size():
+    """
+    The number of nodes in a reconstructed graph should be
+    equal to the number of sensors in the time series data
+    used to reconstruct the graph.
+    """
+    for size in [10, 100, 1000]:
+        T = np.random.random((size, 500))
+        G = reconstruction.CorrelationMatrixReconstructor().fit(T)
+        assert G.order() == size

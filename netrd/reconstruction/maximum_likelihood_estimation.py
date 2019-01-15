4ef9688c9cad1f30551310da4682c4771928a94b
Brennan Klein <brennan@Brennans-MacBook-Pro.local>
2019-01-15 11:40:01 -0500

    add exact mean field and maximum likelihood estimation

---
+++ netrd/reconstruction/maximum_likelihood_estimation.py
@@ -0,0 +1,66 @@
+"""
+maximum_likelihood_estimation.py
+---------------------
+Reconstruction of graphs using maximum likelihood estimation
+author: Brennan Klein
+email: brennanjamesklein at gmail dot com
+submitted as part of the 2019 NetSI Collabathon
+"""
+from .base import BaseReconstructor
+import numpy as np
+import networkx as nx
+
+class MaximumLikelihoodEstimationReconstructor(BaseReconstructor):
+    def fit(self, ts, rate, stop_criterion=True):
+        """
+        Given a (N,t) time series, infer inter-node coupling weights using 
+        maximum likelihood estimation methods. 
+        After [this tutorial]
+        (https://github.com/nihcompmed/network-inference/blob/master/sphinx/codesource/inference.py) 
+        in python.
+        
+        Params
+        ------
+        ts (np.ndarray): Array consisting of $T$ observations from $N$ sensors.
+        rate (float): rate term in maximum likelihood
+        stop_criterion (bool): if True, prevent overly-long runtimes
+        
+        Returns
+        -------
+        G (nx.Graph or nx.DiGraph): a reconstructed graph.
+
+        """
+        
+        N, t = np.shape(ts)             # N nodes, length t
+        rate = rate / t
+
+        s1 = ts[:,:-1]
+        W = np.zeros((N,N))
+
+        nloop = 10000 
+        for i0 in range(N):
+            st1 = ts[i0,1:]             # time series activity of single node
+
+            w    = np.zeros(N)
+            h    = np.zeros(t-1) 
+            cost = np.full(nloop,100.)
+
+            for iloop in range(nloop):
+                dw = np.dot(s1, (st1 - np.tanh(h)))
+
+                w += rate * dw
+                h  = np.dot(s1.T, w)
+
+                cost[iloop] = ((st1 - np.tanh(h))**2).mean()
+
+                if stop_criterion and cost[iloop] >= cost[iloop-1]: 
+                    break
+
+            W[i0,:] = w
+
+        # construct the network
+        self.results['graph'] = nx.from_numpy_array(W)
+        self.results['matrix'] = W
+        G = self.results['graph']
+
+        return G

6b44d843e302d4319d1d23b72f9de7a49173d227
Brennan Klein <brennan@Brennans-MacBook-Pro.local>
2019-01-15 10:54:53 -0500

    add thouless anderson palmer reconstruction

--- netrd/reconstruction/__init__.py
+++ netrd/reconstruction/__init__.py
@@ -3,5 +3,6 @@ from .random import RandomReconstructor
 from .correlation_matrix import CorrelationMatrixReconstructor
 from .free_energy_minimization import FreeEnergyMinimizationReconstructor
 from .naive_mean_field import NaiveMeanFieldReconstructor
+from .thouless_anderson_palmer import ThoulessAndersonPalmerReconstructor
 
 __all__ = []

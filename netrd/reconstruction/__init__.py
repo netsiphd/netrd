b6776bceec765a48b40b2e918b000d4c36367db0
sdmccabe@github.com <stefanmccabe@gmail.com>
2019-01-17 15:40:55 -0500

    Rename CorrelationMatrixReconstructor
    So that we can implement a similar method without regularization, rename
    the current CorrelationMatrixReconstructor, which has regularization, to
    RegluarizedCorrelationMatrixReconstructor

--- netrd/reconstruction/__init__.py
+++ netrd/reconstruction/__init__.py
@@ -1,6 +1,6 @@
 from .base import BaseReconstructor
 from .random import RandomReconstructor
-from .correlation_matrix import CorrelationMatrixReconstructor
+from .regularized_correlation_matrix import RegluarizedCorrelationMatrixReconstructor
 from .free_energy_minimization import FreeEnergyMinimizationReconstructor
 from .naive_mean_field import NaiveMeanFieldReconstructor
 from .thouless_anderson_palmer import ThoulessAndersonPalmerReconstructor

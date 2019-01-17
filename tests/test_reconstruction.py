4f24ad0352619771f09e597b485933455eb9b8ae
sdmccabe@github.com <stefanmccabe@gmail.com>
2019-01-17 15:27:22 -0500

    fix reconstruction tests to be appropriately general

--- tests/test_reconstruction.py
+++ tests/test_reconstruction.py
@@ -8,6 +8,7 @@ Test reconstruction algorithms.
 
 import numpy as np
 from netrd import reconstruction
+from netrd.reconstruction import BaseReconstructor
 
 
 def test_graph_size():
@@ -16,7 +17,9 @@ def test_graph_size():
     equal to the number of sensors in the time series data
     used to reconstruct the graph.
     """
-    for size in [10, 100, 1000]:
-        TS = np.random.random((size, 500))
-        G = reconstruction.CorrelationMatrixReconstructor().fit(TS)
-        assert G.order() == size
+    for obj in reconstruction.__dict__.values():
+        if isinstance(obj, type) and BaseReconstructor in obj.__bases__:
+            for size in [10, 100, 1000]:
+                TS = np.random.random((size, 500))
+                G = obj().fit(TS)
+                assert G.order() == size

3dadfb266504de2ab630a0dc61e602012c2b5e09
Tim LaRock <timothylarock@gmail.com>
2019-01-18 17:27:29 -0500

    I fixed Leo's broken code (his fault).

--- netrd/distance/frobenius.py
+++ netrd/distance/frobenius.py
@@ -26,7 +26,7 @@ class Frobenius(BaseDistance):
         """Frobenius distance between two graphs."""
         adj1 = nx.to_numpy_array(G1)
         adj2 = nx.to_numpy_array(G2)
-        dist = np.linalg.norm((adj1 - adj2).A)
+        dist = np.linalg.norm((adj1 - adj2))
         self.results['dist'] = dist
         self.results['adj'] = np.array([adj1, adj2])
         return dist

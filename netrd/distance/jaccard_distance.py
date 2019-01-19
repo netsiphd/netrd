c43a5555a63e2e2388c2157c3bdb8b1ad708c2cd
Tim LaRock <timothylarock@gmail.com>
2019-01-19 14:37:05 -0500

    Changed jaccard_distance to ensure binary adjacency matrices.

--- netrd/distance/jaccard_distance.py
+++ netrd/distance/jaccard_distance.py
@@ -20,7 +20,7 @@ import numpy as np
 class JaccardDistance(BaseDistance):
     def dist(self, G1, G2):
         """Computes the average jaccard index between two sparse matrices
-        
+
         implementation details here:
         https://scikit-learn.org/stable/modules/model_evaluation.html#jaccard-similarity-score
         and here:
@@ -37,11 +37,10 @@ class JaccardDistance(BaseDistance):
         dist (float): the distance between G1 and G2.
 
         """
-        adj1 = nx.to_numpy_array(G1)
-        adj2 = nx.to_numpy_array(G2)
+        adj1 = nx.to_numpy_array(G1, weight=None)
+        adj2 = nx.to_numpy_array(G2, weight=None)
         dist = 1 - jaccard_similarity_score(adj1,adj2)
         self.results['dist'] = float(dist)
         self.results['adj'] = np.array([adj1, adj2])
         return dist
-
 

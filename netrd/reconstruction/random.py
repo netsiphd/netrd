350ab50c5630718fde98edb7873344da84f6911b
Brennan Klein <brennan@Brennans-MacBook-Pro.local>
2019-01-19 16:41:36 -0500

    change random reconstruction to an elegant, sophisticated random correlation matrix with threshold

--- netrd/reconstruction/random.py
+++ netrd/reconstruction/random.py
@@ -2,17 +2,45 @@
 random.py
 ---------
 
-Reconstruct a random network from time series.
+Reconstruct a network from a random matrix 
+not taking the time series into account.
+
+author: Brennan Klein
+email: klein.br@husky.neu.edu
+Submitted as part of the 2019 NetSI Collabathon.
 
 """
 
 import networkx as nx
+import numpy as np
 from .base import BaseReconstructor
 
 
 class RandomReconstructor(BaseReconstructor):
-    def fit(self, TS):
-        """Reconstruct a random graph."""
-        G = nx.erdos_renyi_graph(TS.shape[0], 0.1)
-        self.results['graph'] = G
+    def fit(self, TS, tau=0.6):
+        """
+        Reconstruct a network from a time serues -- just kidding, simply return 
+        a random correlation matrix with a threshold.
+
+        Params
+        ------
+        TS (np.ndarray): array consisting of $L$ observations from $N$ sensors.
+        tau (float): threshold
+
+        Returns
+        -------
+        G (nx.Graph): a reconstructed graph with $N$ nodes.
+        """
+
+        N, L = TS.shape
+        W = np.random.rand(N, N)
+		A = np.array(W > tau, dtype=int)
+		G = nx.from_numpy_array(A)
+
+		self.results['graph'] = G
+
+        self.results['random_matrix'] = W
+
+        self.results['adjacency_matrix'] = A
+
         return G

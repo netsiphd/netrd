a81fc8a7be713261b09af30a933ba687ce3fba13
Harry Hartle <hth@Harrys-MacBook-Pro.local>
2019-02-18 16:30:39 -0500

    Add Kuramoto dynamics.

--- netrd/dynamics/__init__.py
+++ netrd/dynamics/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseDynamics
 from .sherrington_kirkpatrick import SherringtonKirkpatrickIsing
 from .single_unbiased_random_walker import SingleUnbiasedRandomWalker
-
+from .kuramoto import Kuramoto
 __all__ = []

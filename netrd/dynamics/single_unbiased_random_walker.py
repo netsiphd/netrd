5c29b690aec9680261ed530642e750ad01b892ac
Harry Hartle <hth@Harrys-MacBook-Pro.local>
2019-02-18 14:44:22 -0500

    Fix typos.

--- netrd/dynamics/single_unbiased_random_walker.py
+++ netrd/dynamics/single_unbiased_random_walker.py
@@ -8,40 +8,41 @@ class SingleUnbiasedRandomWalker(BaseDynamics):
         self.results={}
 
     def simulate(self,G,L,initial_node=None):
-    """
-    Simulate single random-walker dynamics on a ground truth network.
-    Generates an N x L time series TS; TS[j,t]==1 if the walker is at
-    node j at time t, and TS[j,t]==0 otherwise.
+        """
+        Simulate single random-walker dynamics on a ground truth network.
+        Generates an N x L time series TS; TS[j,t]==1 if the walker is at
+        node j at time t, and TS[j,t]==0 otherwise.
 
-    Example Usage:
-    #######
-    G = nx.ring_of_cliques(4,16)
-    L = 2001
-    dynamics = SingleUnbiasedRandomWalker()
-    TS = dynamics.simulate(G, L)
-    #######
+        Example Usage:
+        #######
+        G = nx.ring_of_cliques(4,16)
+        L = 2001
+        dynamics = SingleUnbiasedRandomWalker()
+        TS = dynamics.simulate(G, L)
+        #######
 
-    Params
-    ------
-    G (nx.Graph): the input (ground-truth) graph with $N$ nodes.
-    L (int): the length of the desired time series.
+        Params
+        ------
+        G (nx.Graph): the input (ground-truth) graph with $N$ nodes.
+        L (int): the length of the desired time series.
 
-    Returns
-    -------
-    TS (np.ndarray): an $N \times L$ array of synthetic time series data.
-    """
-        # get adjacency matrix and set up vector of indices
-        A=nx.adjacency_matrix(G)
-        N=len(A)
+        Returns
+        -------
+        TS (np.ndarray): an $N \times L$ array of synthetic time series data.
+        """
+    # get adjacency matrix and set up vector of indices
+        A=nx.to_numpy_matrix(G)
+        N=G.number_of_nodes()
         W=np.zeros(L,dtype=int)
         # place walker at initial location
         if initial_node:
             W[0]=initial_node
         else:
             W[0]=np.random.randint(N)
+
         # run dynamical process
         for t in range(L-1):
-            W[t+1]=ra.choice(np.where(A[W[t],:])[0])
+            W[t+1]=np.random.choice(np.where(A[W[t],:])[0])
         self.results['node_index_sequence']=W
         # turn into a binary-valued
         TS=np.zeros((N,L))

7a68cdc97d86920bdfc8082e7ca4831cd3f76ef7
sdmccabe@github.com <stefanmccabe@gmail.com>
2019-04-19 17:41:13 -0400

    Refactor feature_extraction
    1. Refactors the function to be a little more readable.
    2. Corrects the implementation of "number of outgoing edges from neighborhood"
    3. Corrects slight inaccuracy in neighbor clustering and neighbor degree
    features.

--- netrd/distance/netsimile.py
+++ netrd/distance/netsimile.py
@@ -86,35 +86,66 @@ def feature_extraction(G):
 
         node_features (float): the Nx7 matrix of node features."""
 
+    # necessary data structures
     node_features = np.zeros(shape=(G.number_of_nodes(), 7))
-    
     node_list = sorted(G.nodes())
-    
-    # node degrees
     node_degree_dict = dict(G.degree())
-    node_features[:,0] = [node_degree_dict[n] for n in node_list]
-    
-    # clustering coefficient
     node_clustering_dict = dict(nx.clustering(G))
-    node_features[:,1] = [node_clustering_dict[n] for n in node_list]
-    
+    egonets = [nx.ego_graph(G, n) for n in node_list]
+    egonet_dict = {node:egonet for node, egonet in zip(node_list, egonets)}
+
+    # node degrees
+    degs = [node_degree_dict[n] for n in node_list]
+
+    # clustering coefficient
+    clusts = [node_clustering_dict[n] for n in node_list]
+
     # average degree of neighborhood
-    node_features[:,2] = [np.mean([node_degree_dict[m] for m in G.neighbors(n)]) if node_degree_dict[n] > 0 else 0 for n in node_list]
-    
+    neighbor_degs = [
+        np.mean([node_degree_dict[m]
+                 for m in egonets[n].nodes if m != n]) if node_degree_dict[n] > 0 else 0
+        for n in node_list
+    ]
+
     # average clustering coefficient of neighborhood
-    node_features[:,3] = [np.mean([node_clustering_dict[m] for m in G.neighbors(n)]) if node_degree_dict[n] > 0 else 0 for n in node_list]
-    
+    neighbor_clusts = [
+        np.mean([node_clustering_dict[m]
+                 for m in egonets[n].nodes if m != n]) if node_degree_dict[n] > 0 else 0
+        for n in node_list
+    ]
+
     # number of edges in the neighborhood
-    node_features[:,4] = [G.subgraph(list(G.neighbors(n)) + [n]).number_of_edges() if node_degree_dict[n] > 0 else 0 for n in node_list]
-    
+    neighbor_edges = [
+        egonets[n].number_of_edges()
+        if node_degree_dict[n] > 0 else 0 for n in node_list
+    ]
+
     # number of outgoing edges from the neighborhood
     # the sum of neighborhood degrees = 2*(internal edges) + external edges
-    node_features[:,5] = node_features[:,0] * node_features[:,2] - 2*node_features[:,4]
-    
+    #node_features[:,5] = node_features[:,0] * node_features[:,2] - 2*node_features[:,4]
+    neighbor_outgoing_edges = [len([edge for edge in
+                                    set.union(*[set(G.edges(j)) for j in egonets[i].nodes]) if not
+                                    egonets[i].has_edge(*edge)]) for i in node_list]
+
     # number of neighbors of neighbors (not in neighborhood)
-    node_features[:,6] = [len(set([p for m in G.neighbors(n) for p in G.neighbors(m)]) - set(G.neighbors(n)) - set([n])) if node_degree_dict[n] > 0 else 0 for n in node_list]
-    
+    neighbors_of_neighbors = [
+        len(
+            set([p for m in G.neighbors(n)
+                 for p in G.neighbors(m)]) - set(G.neighbors(n)) - set([n]))
+        if node_degree_dict[n] > 0 else 0 for n in node_list
+    ]
+
+    # assembling the features
+    node_features[:, 0] = degs
+    node_features[:, 1] = clusts
+    node_features[:, 2] = neighbor_degs
+    node_features[:, 3] = neighbor_clusts
+    node_features[:, 4] = neighbor_edges
+    node_features[:, 5] = neighbor_outgoing_edges
+    node_features[:, 6] = neighbors_of_neighbors
+
     return np.nan_to_num(node_features)
+
 
 def graph_signature(node_features):
     signature_vec = np.zeros(7*5)

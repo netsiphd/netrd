20d7a86fede979db4fdf61720b1e06b86a76f074
Stefan McCabe <stefanmccabe@gmail.com>
2020-01-13 09:21:11 -0500

    restructure EMD to make license more prominent

--- netrd/distance/nbd.py
+++ netrd/distance/nbd.py
@@ -279,9 +279,6 @@ def half_incidence(graph, ordering='blocks', return_ordering=False):
         return src, tgt
 
 
-def euclidean_distance(x, y):
-    return np.sqrt(sum((a - b) ** 2 for (a, b) in zip(x, y)))
-
 
 def earthmover_distance(p1, p2):
     '''
@@ -294,6 +291,33 @@ def earthmover_distance(p1, p2):
      - p2: an iterable of hashable iterables of numbers (i.e., list of tuples)
 
     '''
+
+    # MIT License
+
+    # Copyright (c) 2020 Jeremy Kun
+
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
+
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+
+    def euclidean_distance(x, y):
+        return np.sqrt(sum((a - b) ** 2 for (a, b) in zip(x, y)))
+
+
     dist1 = {x: float(count) / len(p1) for (x, count) in Counter(p1).items()}
     dist2 = {x: float(count) / len(p2) for (x, count) in Counter(p2).items()}
     solver = pywraplp.Solver(
@@ -339,27 +363,3 @@ def earthmover_distance(p1, p2):
     return objective.Value()
 
 
-# This file borrows code (euclidean_distance and earthmover_distance) with the
-# following license:
-#
-# MIT License
-
-# Copyright (c) 2020 Jeremy Kun
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.

f6f9c77ad331af76edf35654b0af563fa2e2f86a
Stefan McCabe <stefanmccabe@gmail.com>
2020-01-15 17:43:01 -0500

    Refactor Kuramoto model
    This commit provides a general refactor of the Kuramoto model. The changes are
    as following:
    
    - normalize phase states by 2*pi to make synchronization behavior clearer
    - rename parameters to ease interpretation
    - provide some error checking for parameters setting initial states of nodes
    - more documentation of Kuramoto process
    - rename variables for consistency with standard reference on Kuramoto models

--- netrd/dynamics/kuramoto.py
+++ netrd/dynamics/kuramoto.py
@@ -1,6 +1,7 @@
 """
 kuramoto.py
 -----------
+Kuramoto model of oscillators.
 
 author: Harrison Hartle
 """
@@ -17,15 +18,34 @@ class Kuramoto(BaseDynamics):
     def __init__(self):
         self.results = {}
 
-    def simulate(self, G, L, dt=0.01, K=1, ICs=[], freqs=[]):
+    def simulate(self, G, L, dt=0.01, strength=1, phases=None, freqs=None):
         r"""Simulate Kuramoto model on a ground truth network.
+
+        Kuramoto oscillators model synchronization processes. At each time
+        step, each node adjusts its phase :math:`\theta_i` according to the
+        equation
+
+        .. math::
+            \theta_i = \omega_i + \frac{\lambda}{N}\sum_{j=1}^{N}\sin\left(\theta_j - \theta_i\right),
+
+
+        where :math:`\lambda`, is a coupling `strength` parameter and each node
+        has an internal frequency :math:`\omega_i`; the `freqs` function
+        parameter provides the option to initialize these frequencies with
+        user-defined values (or leave as `None` to randomly initialize). Each
+        node's initial phase :math:`\theta_{i0}` can be randomly initialized
+        (the default behavior) or set by specifying the `phases` parameter.
 
         The results dictionary also stores the ground truth network as
         `'ground_truth'` and the internal frequencies of the process as
         `'internal_frequencies'`.
 
+        For more information on the Kuramoto model, see the review essay
+        included below.
+
         Parameters
         ----------
+
         G (nx.Graph)
             the input (ground-truth) graph with :math:`N` nodes.
 
@@ -35,61 +55,80 @@ class Kuramoto(BaseDynamics):
         dt (float)
             size of timestep for numerical integration.
 
-        K (float)
+        strength (float)
             coupling strength (prefactor for interaction terms).
 
-        ICs (np.ndarray)
-            an :math:`N \times 1` array of initial phase-angles.
+        phases (np.ndarray)
+            an :math:`N \times 1` array of initial phases.
 
         freqs (np.ndarray)
             an :math:`N \times 1` array of internal frequencies.
 
         Returns
         -------
+
         TS (np.ndarray)
             an :math:`N \times L` array of synthetic time series data.
 
         Examples
         --------
+
         .. code:: python
 
             G = nx.ring_of_cliques(4,16)
+            N = G.number_of_nodes()
             L = int(1e4)
+            omega = np.random.uniform(0.95, 1.05, N)
             dynamics = Kuramoto()
-            TS = dynamics.simulate(G, L, dt=0.04, K=0.3)
+            TS = dynamics.simulate(G, L, dt=0.01, strength=0.3, freqs=omega)
 
+        References
+        ----------
+        .. [1] F. Rodrigues, T. Peron, P. Ji, J. Kurths.
+               The Kuramoto model in complex networks.
+               https://arxiv.org/abs/1511.07139
 
         """
-        # get network features
         A = nx.to_numpy_array(G)
         N = G.number_of_nodes()
 
-        # setup for running the evolution equations
-        psi0 = (
-            ICs if len(ICs) > 0 else 2.0 * np.pi * np.random.rand(N)
-        )  # set initial conditions
-        g = (
-            freqs if len(freqs) > 0 else 0.1 + 0.100 * np.random.rand(N)
-        )  # set internal frequency vector
+        try:
+            if phases is not None and len(phases) == N:
+                theta_0 = phases
+            else:
+                theta_0 = 2 * np.pi * np.random.rand(N)
+
+            if freqs is not None and len(freqs) == N:
+                omega = freqs
+            else:
+                omega = np.random.uniform(0.9, 1.1, N)
+
+        except AssertionError:
+            raise ValueError("Initial conditions must be None or " "lists of length N.")
 
         t = np.linspace(dt, L * dt, L)  # time-vector
-        o = np.ones(N)  # define a rate of change function
+        one = np.ones(N)  # define a rate of change function
 
-        ddt_psis = lambda psi, t, g, K, A: g + (K / N) * (
-            A * np.sin(np.outer(o, psi) - np.outer(psi, o))
-        ).dot(o)
+        def ddt_theta(theta, t, g, strength, A):
+            prefactor = strength / N
+            first = np.outer(one, theta)
+            second = np.outer(theta, one)
 
-        argu = (g, K, A)  # arguments in a tuple for it.integrate
+            return g + prefactor * (A * np.sin(first - second)).dot(one)
+
         # integrate the equations of motion numerically
-        TS_T = it.odeint(ddt_psis, psi0, t, args=argu)
+        args = (omega, strength, A)
+        TS_T = it.odeint(ddt_theta, theta_0, t, args=args)
 
-        # odeint returns LxN result - transposing yields reversed-order nodes => apply flipud.
+        # odeint returns LxN result
+        # transposing yields reversed-order nodes => apply flipud.
         TS = np.flipud(TS_T.T)
 
-        self.results[
-            'internal_frequencies'
-        ] = g  # save the internal-frequency vector to results
-        self.results['ground_truth'] = G  # save the ground-truth network to results
-        self.results['TS'] = TS  # save the timeseries data to results
+        # adjust phases
+        TS = TS % (2 * np.pi)
+
+        self.results["internal_frequencies"] = omega
+        self.results["ground_truth"] = G
+        self.results["TS"] = TS
 
         return TS

0233efe0118c14272aa1d5f7855f379543f4cf7b
Brennan Klein <klein.br@husky.neu.edu>
2020-01-15 13:34:28 -0500

    new granger causality that works (#278)
    * update granger causality with Charles's new code
    
    * finalize time granger causality

--- netrd/reconstruction/time_granger_causality.py
+++ netrd/reconstruction/time_granger_causality.py
@@ -6,15 +6,14 @@ Graph reconstruction algorithm based on [1].
 
 [1] P. Desrosiers, S. Labrecque, M. Tremblay, M. Bélanger, B. De Dorlodot,
 D. C. Côté, "Network inference from functional experimental data", Proc. SPIE
-9690, Clinical and Translational Neurophotonics; Neural Imaging and Sensing; and
-Optogenetics and Optical Manipulation, 969019 (2016);
+9690, Clinical and Translational Neurophotonics; Neural Imaging and Sensing;
+and Optogenetics and Optical Manipulation, 969019 (2016);
 
 author: Charles Murphy
 email: charles.murphy.1@ulaval.ca
 Submitted as part of the 2019 NetSI Collabathon.
 """
 
-import networkx as nx
 import numpy as np
 
 from .base import BaseReconstructor
@@ -23,107 +22,111 @@ from ..utilities import create_graph, threshold
 
 
 class TimeGrangerCausality(BaseReconstructor):
-    """Infers Granger causality."""
+    """Uses the Granger causality between nodes."""
 
-    def fit(self, TS, lag=1, threshold_type='range', **kwargs):
-        """Reconstruct a network based on the Granger causality.
+    def fit(self, TS, lag=1, threshold_type="range", **kwargs):
+        r"""Reconstruct a network based on the Granger causality. To evaluate
+        the effect of a time series :math:`j` over another, :math:`i`, it first
+        evaluates the error :math:`e_1` given by an autoregressive model fit
+        with :math:`i` alone. Then, it evaluates another error :math:`e_2`
+        given by an autoregressive model trained to correlate the future of
+        :math:`i` with the past of :math:`i` and :math:`j`. The Granger
+        causality of node :math:`j` over :math:`i` is simply given by
+        :math:`log(var(e_1) / var(e_2))``.
 
-        To evaluate the effect of a time series (j) over another (i), it
-        first evaluates the error e2 given by an autoregressive model
-        fitted to (i) alone. Then, it evaluates another error e2 given by
-        an autoregressive model training to correlate the future of (i)
-        with the past of (i) and (j). The Granger causality of node (j)
-        over (i) is simply given by :math:`\log(var(e1) / var(e2))`. It
-        constructs the network by calculating the Granger causality for
-        each pair of nodes.
-
-        The results dictionary also stores the weight matrix as
-        `'weights_matrix'` and the thresholded version of the weight matrix
-        as `'thresholded_matrix'`.
+        It reconstructs the network by calculating the Granger
+        causality for each pair of nodes.
 
         Parameters
         ----------
+
         TS (np.ndarray)
             Array consisting of :math:`L` observations from :math:`N`
             sensors.
 
         lag (int)
-            Time lag.
+            Time lag to consider.
 
         threshold_type (str)
             Which thresholding function to use on the matrix of
-
-        weights
-            See `netrd.utilities.threshold.py` for documentation. Pass
-            additional arguments to the thresholder using ``**kwargs``.
+            weights. See `netrd.utilities.threshold.py` for
+            documentation. Pass additional arguments to the thresholder
+            using ``**kwargs``.
 
         Returns
-        -------
+        --------
 
         G (nx.Graph)
             A reconstructed graph with :math:`N` nodes.
 
-        References
-        ----------
-        .. [1] P. Desrosiers, S. Labrecque, M. Tremblay, M. Bélanger, B. De
-               Dorlodot, D. C. Côté, "Network inference from functional
-               experimental data", Proc. SPIE 9690, Clinical and
-               Translational Neurophotonics; Neural Imaging and Sensing;
-               and Optogenetics and Optical Manipulation, 969019 (2016);
-
         """
-        n = TS.shape[0]
-        self.results['weights_matrix'] = np.zeros([n, n])
-        for i in range(n):
-            xi, yi = get_training_data(TS[i, :], lag)
-            for j in range(n):
-                xj, yj = get_training_data(TS[j, :], lag)
-                X, Y = np.concatenate([xi, xj]), np.concatenate([yi, yj])
-                reg1 = LinearRegression().fit(xi, yi)
-                reg2 = LinearRegression().fit(X, Y)
-                err1 = yi - reg1.predict(xi)
-                err2 = Y - reg2.predict(X)
-                self.results['weights_matrix'][j, i] = np.log(
-                    np.std(err1) / np.std(err2)
-                )
 
+        n = TS.shape[0]
+        W = np.zeros([n, n])
+
+        for i in range(n):
+            xi, yi = TimeGrangerCausality.split_data(TS[i, :], lag)
+
+            for j in range(n):
+                xj, yj = TimeGrangerCausality.split_data(TS[j, :], lag)
+                xij = np.concatenate([xi, xj], axis=-1)
+                reg1 = LinearRegression().fit(xi, yi)
+                reg2 = LinearRegression().fit(xij, yi)
+                err1 = yi - reg1.predict(xi)
+                err2 = yi - reg2.predict(xij)
+
+                std_i = np.std(err1)
+                std_ij = np.std(err2)
+
+                if std_i == 0:
+                    W[j, i] = -99999999
+                elif std_ij == 0:
+                    W[j, i] = 99999999
+                else:
+                    W[j, i] = np.log(std_i) - np.log(std_ij)
+
+        self.results["weights_matrix"] = W
         # threshold the network
-        W_thresh = threshold(self.results['weights_matrix'], threshold_type, **kwargs)
-        self.results['thresholded_matrix'] = W_thresh
+        W_thresh = threshold(W, threshold_type, **kwargs)
+        self.results["thresholded_matrix"] = W_thresh
 
         # construct the network
-        self.results['graph'] = create_graph(W_thresh)
-        G = self.results['graph']
+        self.results["graph"] = create_graph(W_thresh)
+        G = self.results["graph"]
 
         return G
 
+    @staticmethod
+    def split_data(TS, lag):
+        """From a single node time series, return a training dataset with
+        corresponding targets.
 
-def get_training_data(TS, lag):
-    """From a single node time series, it returns a training dataset with
-    corresponding targets.
+        Parameters
+        ----------
 
-    Parameters
-    ----------
+        TS (np.ndarray)
+            Array consisting of :math:`L` observations from :math:`N`
+            sensors.
 
-    TS (np.ndarray)
-        Array consisting of :math:`L` observations from :math:`N` sensors.
+        lag (int)
+            Time lag to consider.
 
-    lag (int): Time lag.
+        Returns
+        -------
 
-    Returns
-    -------
+        inputs (np.ndarray)
+            Training data for the inputs.
 
-    inputs (np.ndarray): Training data for the inputs.
+        targets (np.ndarray)
+            Training data for the targets.
 
-    targets (np.ndarray): Training data for the targets.
+        """
+        T = len(TS)
+        inputs = np.zeros([T - lag - 1, lag])
+        targets = np.zeros(T - lag - 1)
 
-    """
-    T = len(TS)
-    inputs = np.zeros([T - lag - 1, lag])
-    targets = np.zeros(T - lag - 1)
+        for t in range(T - lag - 1):
+            inputs[t, :] = TS[t : lag + t]
+            targets[t] = TS[t + lag]
 
-    for t in range(T - lag - 1):
-        inputs[t, :] = TS[t : lag + t]
-        targets[t] = TS[t + lag + 1]
-
-    return inputs, targets
+        return inputs, targets
